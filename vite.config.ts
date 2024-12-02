80ab65acc8a896f301d514b7c2bbb7d9ffd89f92
Andre Ekbom <andreekbom@live.se>
2024-12-02 09:47:38 +0100

    Update build

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,38 @@
-import { resolve } from 'path';
+import { resolve, isAbsolute } from 'path';
+import { copyFileSync, renameSync } from 'node:fs';
 import preserveDirectives from 'rollup-preserve-directives';
-import { defineConfig, Plugin } from 'vite';
+import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [
-    preserveDirectives() as Plugin,
-    dts({ include: 'src', exclude: ['**/*.test.ts'], insertTypesEntry: true }),
+    preserveDirectives(),
+    dts({
+      include: 'src',
+      exclude: ['**/*.test.ts'],
+      insertTypesEntry: true,
+      rollupTypes: true,
+      afterBuild: () => {
+        copyFileSync('dist/index.d.ts', 'dist/index.d.cts');
+        renameSync('dist/index.d.ts', 'dist/index.d.mts');
+      },
+    }),
     tsconfigPaths(),
   ],
   build: {
     copyPublicDir: false,
+    minify: true,
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
-      fileName: (format, entryName) => `${entryName}.${format === 'es' ? 'js' : format}`,
+      fileName: (format, entryName) => `${entryName}.${format === 'es' ? 'mjs' : format}`,
       formats: ['es', 'cjs'],
     },
     rollupOptions: {
       output: {
         preserveModules: true,
       },
+      external: (id) => !(isAbsolute(id) || id.startsWith('.')),
     },
   },
 });
